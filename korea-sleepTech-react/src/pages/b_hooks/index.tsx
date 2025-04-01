d07ae551974b05a2338a51964cffa2fe602581d7
buzz9248 <buzz9248@naver.com>
2025-04-01 10:33:21 +0900

    react basic

---
+++ korea-sleepTech-react/src/pages/b_hooks/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+function Index() {
+  return (
+    <div>
+      <h1
+        style={{
+          backgroundColor: "black",
+          color: "pink",
+        }}
+      >
+        === 리액트 Hooks ===
+      </h1>
+    </div>
+  )
+}
+
+export default Index
